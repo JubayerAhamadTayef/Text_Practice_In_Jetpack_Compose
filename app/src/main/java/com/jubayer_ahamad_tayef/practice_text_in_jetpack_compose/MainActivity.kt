3efbcafd76431ff1e4098af39889ec52e7b20070
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-19 07:28:48 +0600

    Create a new empty compose android project!

---
+++ app/src/main/java/com/jubayer_ahamad_tayef/practice_text_in_jetpack_compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jubayer_ahamad_tayef.practice_text_in_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jubayer_ahamad_tayef.practice_text_in_jetpack_compose.ui.theme.Practice_Text_In_Jetpack_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practice_Text_In_Jetpack_ComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Practice_Text_In_Jetpack_ComposeTheme {
+        Greeting("Android")
+    }
+}
